7ff2bd1784badfba37852ae025d2074a0f486aef
sethvincent <sethvincent@gmail.com>
2015-01-07 16:34:05 -0800

    allow dat path when creating datServer

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function (dat) {
+module.exports = function (dir, opts, onReady) {
+  if (typeof dat === 'string') var dat = require('dat')(dir, opts, onReady)
+
   var handler = require('./components/handler')(dat)
   var router = require('./components/router')(handler)
   var server = require('./components/server')(router)
