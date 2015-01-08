c07f99e13976ac54a73d12582527f8d8df0efe46
sethvincent <sethvincent@gmail.com>
2015-01-07 17:09:19 -0800

    trying to allow dat path or dat instance but does not work yet

--- example.js
+++ example.js
@@ -1,6 +1,19 @@
+/*
+  
+
+
 var dat = require('dat')('./db', function () {
-  var server = require('./')(dat);
-  server.listen(3333, function () {
+  var server = require('./')(dat)
+  
+  server.listen(3333, function() {
     console.log('Listening at 127.0.0.1:3333')
   })
 })
+
+*/
+
+var server = require('./')('./db', {}, function () {
+  server.listen(3333, function () {
+    console.log('Listening at 127.0.0.1:3333')
+  })
+});
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-module.exports = function (dir, opts, onReady) {
-  if (typeof dat === 'string') var dat = require('dat')(dir, opts, onReady)
+var Dat = require('dat')
 
+var restHandler = require('./components/handler')
+var restRouter = require('./components/router')
+var restServer = require('./components/server')
+
+module.exports = function (dir, opts, onReady) {
+  // if dir is actuall a dat instance
+  if (dir.exists) return createServer(dir)
+
+  // else we're passing options to create a dat
+  else {
+    var dat = Dat(dir, opts, onReady)
+    return createServer(dat)
+  }
+}
+
+function createServer (dat) {
   var handler = require('./components/handler')(dat)
   var router = require('./components/router')(handler)
   var server = require('./components/server')(router)
 
   return server
 }
-
 
